521cb81f52748db3aaa5dfefab2f1acf5608efe9
Vadim Shidlov <mat23vadim@mail.ru>
2023-03-29 22:10:25 +0300

    fix: add favicon and change some active effects links

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,15 +1,1 @@
-//class active header links
-
-const headerItemsLinks = document.querySelectorAll('.header__menu-item');
-console.log(headerItemsLinks);
-
-headerItemsLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        headerItemsLinks.forEach(el => {
-            el.classList.remove('header__menu-item_active');
-        });
-        link.classList.add('header__menu-item_active')
-    })
-
-})
 
