b1f071ccc0ce184516a7e28b5a14f3d6f17ac5b4
Vadim Shidlov <mat23vadim@mail.ru>
2023-04-02 14:15:31 +0300

    feat: create constants for burger-menu

---
+++ shelter/pages/main/modal-window.js
@@ -0,0 +1,7 @@
+//  ---------BURGER-MENU__________________
+
+const blockHouse = document.querySelector('.block-house');
+const burgerBtn = document.querySelector('.header__burger-button');
+const headerMenu = document.querySelector('.header__menu.header__menu-item_active');
+const burgerBody = document.querySelector('.header__menu');
+const headerLinks = document.querySelectorAll('.header__menu-item');
