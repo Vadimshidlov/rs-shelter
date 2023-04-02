549cfa6aef226cb911f48b7e2f42f0e45211f645
Vadim Shidlov <mat23vadim@mail.ru>
2023-04-02 14:27:16 +0300

    feat: create functions showBurger and hideBurger

--- shelter/pages/main/modal-window.js
+++ shelter/pages/main/modal-window.js
@@ -5,3 +5,21 @@ const burgerBtn = document.querySelector('.header__burger-button');
 const headerMenu = document.querySelector('.header__menu.header__menu-item_active');
 const burgerBody = document.querySelector('.header__menu');
 const headerLinks = document.querySelectorAll('.header__menu-item');
+
+function showBurger() {
+    document.body.classList.toggle('stop-scrolling');
+    blockHouse.classList.toggle('burger_opacity');
+    burgerBtn.classList.toggle('header__burger-button_active');
+    burgerBody.classList.toggle('header__menu-item_active');
+}
+function hideBurger() {
+    blockHouse.classList.remove('burger_opacity');
+    burgerBody.classList.remove('header__menu-item_active');
+    burgerBtn.classList.toggle('header__burger-button_active');
+    document.body.classList.remove('stop-scrolling');
+}
+
+burgerBtn.addEventListener('click', () => {
+    console.log('hello');
+    showBurger();
+});
