b2927b15001a3673aa7c1a3611818257d152b31f
Vadim Shidlov <mat23vadim@mail.ru>
2023-04-02 17:08:02 +0300

    feat: add eventlisteners for burger-menu elemets and header-links

--- shelter/pages/main/modal-window.js
+++ shelter/pages/main/modal-window.js
@@ -2,7 +2,7 @@
 
 const blockHouse = document.querySelector('.block-house');
 const burgerBtn = document.querySelector('.header__burger-button');
-const headerMenu = document.querySelector('.header__menu.header__menu-item_active');
+// const headerMenu = document.querySelector('.header__menu.header__menu-item_active');
 const burgerBody = document.querySelector('.header__menu');
 const headerLinks = document.querySelectorAll('.header__menu-item');
 
@@ -13,13 +13,31 @@ function showBurger() {
     burgerBody.classList.toggle('header__menu-item_active');
 }
 function hideBurger() {
-    blockHouse.classList.remove('burger_opacity');
-    burgerBody.classList.remove('header__menu-item_active');
+    blockHouse.classList.toggle('burger_opacity');
+    burgerBody.classList.toggle('header__menu-item_active');
     burgerBtn.classList.toggle('header__burger-button_active');
-    document.body.classList.remove('stop-scrolling');
+    document.body.classList.toggle('stop-scrolling');
 }
 
 burgerBtn.addEventListener('click', () => {
     console.log('hello');
     showBurger();
+});
+
+window.addEventListener('click', e => {
+    console.log(e.target);
+    if (burgerBody.classList.contains('header__menu-item_active')) {
+        console.log(blockHouse);
+        blockHouse.childNodes.forEach(el => {
+            el.addEventListener('click', () => {
+                hideBurger();
+            });
+        });
+
+        headerLinks.forEach(el => {
+            el.addEventListener('click', () => {
+                hideBurger();
+            });
+        });
+    }
 });
