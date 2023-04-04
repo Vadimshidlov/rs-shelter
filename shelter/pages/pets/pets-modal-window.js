4e0b5c97beaacd71489d22def31e63fff55633c7
Vadim Shidlov <mat23vadim@mail.ru>
2023-04-04 21:21:12 +0300

    feat: create constants for burger-menu at page pets

---
+++ shelter/pages/pets/pets-modal-window.js
@@ -0,0 +1,8 @@
+// --------BURGER-MENU--------
+
+const blockFriends = document.querySelector('.block-friends');
+const headerContent = document.querySelector('.header_content');
+const burgerBtn = document.querySelector('.header__burger-button');
+const headerMenu = document.querySelector('.header__menu.header__menu-item_active');
+const burgerBody = document.querySelector('.header__menu');
+const headerLinks = document.querySelectorAll('.header__menu-item');
