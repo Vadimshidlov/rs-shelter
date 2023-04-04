2b937e5dcd852dc0b53d959cc28277bcc5445e92
Vadim Shidlov <mat23vadim@mail.ru>
2023-04-04 23:09:09 +0300

    fix: fix block friends constants at page pets

--- shelter/pages/pets/pets-modal-window.js
+++ shelter/pages/pets/pets-modal-window.js
@@ -28,11 +28,10 @@ burgerBtn.addEventListener('click', () => {
 window.addEventListener('click', e => {
     console.log(e.target);
     if (burgerBody.classList.contains('header__menu-item_active')) {
-        console.log(blockHouse);
-        blockHouse.childNodes.forEach(el => {
+        blockFriends.childNodes.forEach(el => {
             el.addEventListener('click', () => {
                 document.body.classList.toggle('stop-scrolling');
-                blockHouse.classList.toggle('burger_opacity');
+                blockFriends.classList.toggle('burger_opacity');
                 burgerBtn.classList.toggle('header__burger-button_active');
                 burgerBody.classList.toggle('header__menu-item_active');
             });
@@ -44,7 +43,7 @@ headerLinks.forEach(el => {
     el.addEventListener('click', () => {
         burgerBody.classList.remove('header__menu-item_active');
         burgerBtn.classList.remove('header__burger-button_active');
-        blockHouse.classList.toggle('burger_opacity');
+        blockFriends.classList.toggle('burger_opacity');
         document.body.classList.toggle('stop-scrolling');
     });
 });
