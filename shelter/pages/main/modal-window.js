8ffe338b5e0055ba2e33e8b689155553ed3c0ab8
Vadim Shidlov <mat23vadim@mail.ru>
2023-04-04 16:47:37 +0300

    fix: fixed structure of html markup modal-window and and styles for modal window

--- shelter/pages/main/modal-window.js
+++ shelter/pages/main/modal-window.js
@@ -215,3 +215,15 @@ function hideModal() {
     modalWindow.classList.remove("block-friends__modal-window_active");
     document.body.classList.remove("stop-scrolling");
 }
+
+window.addEventListener("click", (e) => {
+    console.log(e.target);
+    if (e.target === modalWindow) {
+        hideModal();
+    }
+});
+window.addEventListener("keydown", (e) => {
+    if (e.keyCode === 27) {
+        hideModal();
+    }
+});
