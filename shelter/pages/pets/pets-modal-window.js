81e0516afd2757af8aaea75e40186e7dbeb900de
Vadim Shidlov <mat23vadim@mail.ru>
2023-04-05 10:54:30 +0300

    fix: fixed style for burger-menu and fixed constants for burger-menu at page pets

--- shelter/pages/pets/pets-modal-window.js
+++ shelter/pages/pets/pets-modal-window.js
@@ -1,49 +1,53 @@
 // --------BURGER-MENU--------
 
-const blockFriends = document.querySelector('.block-friends');
-const headerContent = document.querySelector('.header_content');
-const burgerBtn = document.querySelector('.header__burger-button');
-const headerMenu = document.querySelector('.header__menu.header__menu-item_active');
-const burgerBody = document.querySelector('.header__menu');
-const headerLinks = document.querySelectorAll('.header__menu-item');
+const blockFriends = document.querySelector(".block-friends");
+const headerContent = document.querySelector(".header_content");
+const burgerBtn = document.querySelector(".header__burger-button");
+const headerMenu = document.querySelector(
+    ".header__menu.header__menu-item_active"
+);
+const burgerBody = document.querySelector(".header__menu");
+const headerLinks = document.querySelectorAll(".header__menu-item");
+const headerLogo = document.querySelector(".header__logo");
 
 function showBurger() {
-    document.body.classList.toggle('stop-scrolling');
-    blockFriends.classList.toggle('burger_opacity');
-    burgerBtn.classList.toggle('header__burger-button_active');
-    burgerBody.classList.toggle('header__menu-item_active');
+    document.body.classList.toggle("stop-scrolling");
+    blockFriends.classList.toggle("burger_opacity");
+    headerLogo.classList.toggle("burger_opacity");
+    burgerBtn.classList.toggle("header__burger-button_active");
+    burgerBody.classList.toggle("header__menu-item_active");
 }
 function hideBurger() {
-    blockFriends.classList.remove('burger_opacity');
-    burgerBody.classList.remove('header__menu-item_active');
-    burgerBtn.classList.toggle('header__burger-button_active');
-    document.body.classList.remove('stop-scrolling');
+    blockFriends.classList.remove("burger_opacity");
+    burgerBody.classList.remove("header__menu-item_active");
+    burgerBtn.classList.toggle("header__burger-button_active");
+    document.body.classList.remove("stop-scrolling");
 }
 
-burgerBtn.addEventListener('click', () => {
-    console.log('hello');
+burgerBtn.addEventListener("click", () => {
+    console.log("hello");
     showBurger();
 });
 
-window.addEventListener('click', e => {
+window.addEventListener("click", (e) => {
     console.log(e.target);
-    if (burgerBody.classList.contains('header__menu-item_active')) {
-        blockFriends.childNodes.forEach(el => {
-            el.addEventListener('click', () => {
-                document.body.classList.toggle('stop-scrolling');
-                blockFriends.classList.toggle('burger_opacity');
-                burgerBtn.classList.toggle('header__burger-button_active');
-                burgerBody.classList.toggle('header__menu-item_active');
+    if (burgerBody.classList.contains("header__menu-item_active")) {
+        blockFriends.childNodes.forEach((el) => {
+            el.addEventListener("click", () => {
+                document.body.classList.toggle("stop-scrolling");
+                blockFriends.classList.toggle("burger_opacity");
+                burgerBtn.classList.toggle("header__burger-button_active");
+                burgerBody.classList.toggle("header__menu-item_active");
             });
         });
     }
 });
 
-headerLinks.forEach(el => {
-    el.addEventListener('click', () => {
-        burgerBody.classList.remove('header__menu-item_active');
-        burgerBtn.classList.remove('header__burger-button_active');
-        blockFriends.classList.toggle('burger_opacity');
-        document.body.classList.toggle('stop-scrolling');
+headerLinks.forEach((el) => {
+    el.addEventListener("click", () => {
+        burgerBody.classList.remove("header__menu-item_active");
+        burgerBtn.classList.remove("header__burger-button_active");
+        blockFriends.classList.toggle("burger_opacity");
+        document.body.classList.toggle("stop-scrolling");
     });
 });
