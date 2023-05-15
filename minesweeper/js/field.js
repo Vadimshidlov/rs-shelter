307979d8cd46eb6585ea12a3f7103653eceaf540
Vadim Shidlov <mat23vadim@mail.ru>
2023-05-15 15:27:09 +0300

    fix: fixed function getStateArroundItems

--- minesweeper/js/field.js
+++ minesweeper/js/field.js
@@ -1,21 +1,21 @@
 // import { getItem } from "./fieldItem.js";
 
-let matrixField = [];
-
 function getRandomNubers(max, min) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
 function getMatrixField(width = 10, height = 10, bombs = 10) {
+  const arr = [];
   for (let i = 0; i < height; i++) {
     let curr = [];
     for (let j = 0; j < width; j++) {
       curr[j] = 0;
     }
-    matrixField.push(curr);
+    arr.push(curr);
   }
 
-  getBobms(bombs);
+  const arrayWithBobms = getBobms(arr, bombs);
+  console.log(arrayWithBobms);
 
   // for (let i = 0; i < matrixField.length; i++) {
   //   for (let j = 0; j < matrixField[i].length; j++) {
@@ -26,46 +26,71 @@ function getMatrixField(width = 10, height = 10, bombs = 10) {
   //   }
   // }
 
-  return matrixField;
+  arrayWithBobms.forEach((el, x) => {
+    el.forEach((el, y) => {
+      if (el !== "b") {
+        getStateOfArroundItems(arrayWithBobms, { x, y });
+      }
+    });
+  });
+
+  return arrayWithBobms;
 }
 
-function getBobms(bombs) {
+function getBobms(arr, bombs) {
+  const arrayWithBobms = arr;
   let bombsCount = bombs;
-  const matrixFieldHeight = matrixField.length;
-  const matrixFieldWidth = matrixField[0].length;
+  const matrixFieldHeight = arr.length;
+  const matrixFieldWidth = arr[0].length;
 
   while (bombsCount) {
     const x = getRandomNubers(0, matrixFieldWidth - 1);
     const y = getRandomNubers(0, matrixFieldHeight);
-    let current = matrixField[y][x];
+    let current = arr[y][x];
 
     if (!current) {
-      matrixField[y][x] = 1;
+      arr[y][x] = "b";
       bombsCount--;
     }
   }
+
+  return arrayWithBobms;
 }
 
-function getStateOfArroundItems(palce) {
+function getStateOfArroundItems(arr, palce) {
   const { x, y } = palce;
+  let count = 0;
 
-  const item_1 = matrixField[y]?.[x - 1];
-  const item_2 = matrixField[y - 1]?.[x - 1];
-  const item_3 = matrixField[y - 1]?.[x];
-  const item_4 = matrixField[y - 1]?.[x + 1];
-  const item_5 = matrixField[y]?.[x + 1];
-  const item_6 = matrixField[y + 1]?.[x + 1];
-  const item_7 = matrixField[y + 1]?.[x];
-  const item_8 = matrixField[y + 1]?.[x - 1];
+  const item_1 = arr[x]?.[y - 1];
+  const item_2 = arr[x - 1]?.[y - 1];
+  const item_3 = arr[x - 1]?.[y];
+  const item_4 = arr[x - 1]?.[y + 1];
+  const item_5 = arr[x]?.[y + 1];
+  const item_6 = arr[x + 1]?.[y + 1];
+  const item_7 = arr[x + 1]?.[y];
+  const item_8 = arr[x + 1]?.[y - 1];
 
-  const arr = [item_1, item_2, item_3, item_4, item_5, item_6, item_7, item_8];
-  arr.filter((item) => typeof item !== "undefined");
-  const count = arr.reduce((acc, el, i) => {
-    acc += el === 1 ? 1 : 0;
+  const arrayArround = [
+    item_1,
+    item_2,
+    item_3,
+    item_4,
+    item_5,
+    item_6,
+    item_7,
+    item_8,
+  ].filter((item) => typeof item !== "undefined");
+  // console.log(arrayArround);
+
+  count = arrayArround.reduce((acc, el, i) => {
+    acc += el === "b" ? 1 : 0;
     return acc;
   }, 0);
-  matrixField[x][y] = count;
+  arr[x][y] = count;
 }
 
-console.log(getMatrixField(3, 3, 3));
-console.log(getStateOfArroundItems({ x: 1, y: 1 }));
+let matrixField = getMatrixField(10, 10, 8);
+console.log(matrixField);
+
+// console.log(getStateOfArroundItems({ x: 1, y: 1 }));
+// console.log(matrixField);
