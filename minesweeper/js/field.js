6789df9432cecf795f9805478e9e841e0704bdcd
Vadimshidlov <mat23vadim@mail.ru>
2023-05-17 07:20:17 +0300

    feat: create method moveToBattlefield and create dataset object for item field

--- minesweeper/js/field.js
+++ minesweeper/js/field.js
@@ -1,111 +1,10 @@
-// import { getItem } from "./fieldItem.js";
-import { itemField } from "./fieldItem.js";
-import { App } from "./app-page.js";
-// const field = document.querySelector(".body__field");
-// console.log(field);
+import { itemField } from './fieldItem.js';
+import { App } from './app-page.js';
 
 export function getRandomNubers(max, min) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-/* 
-
-export function getMatrixField(width = 10, height = 10, bombs = 10) {
-  const arr = [];
-  for (let i = 0; i < height; i++) {
-    let curr = [];
-    for (let j = 0; j < width; j++) {
-      const item = document.createElement("div");
-      curr[j] = new itemField(false, { i, j });
-    }
-    arr.push(curr);
-  }
-
-  const arrayWithBobms = getBobms(arr, bombs);
-
-  arrayWithBobms.forEach((el, x) => {
-    el.forEach((el, y) => {
-      if (el.content !== "b") {
-        getStateOfArroundItems(arrayWithBobms, { x, y });
-      }
-    });
-  });
-
-  return arrayWithBobms;
-}
-
-export function getBobms(arr, bombs) {
-  const arrayWithBobms = arr;
-  console.log(arr);
-  let bombsCount = bombs;
-  const matrixFieldHeight = arr.length;
-  const matrixFieldWidth = arr[0].length;
-
-  while (bombsCount) {
-    const x = getRandomNubers(0, matrixFieldWidth - 1);
-    const y = getRandomNubers(0, matrixFieldHeight);
-    let current = arr[y][x];
-
-    if (!current.isBomb) {
-      arr[y][x].content = "b";
-      arr[y][x].isBomb = true;
-      bombsCount--;
-    } else {
-      arr[y][x].content = 0;
-    }
-  }
-
-  return arrayWithBobms;
-}
-
-export function getStateOfArroundItems(arr, palce) {
-  const { x, y } = palce;
-  let count = 0;
-
-  const item_1 = arr[x]?.[y - 1];
-  const item_2 = arr[x - 1]?.[y - 1];
-  const item_3 = arr[x - 1]?.[y];
-  const item_4 = arr[x - 1]?.[y + 1];
-  const item_5 = arr[x]?.[y + 1];
-  const item_6 = arr[x + 1]?.[y + 1];
-  const item_7 = arr[x + 1]?.[y];
-  const item_8 = arr[x + 1]?.[y - 1];
-
-  const arrayArround = [
-    item_1,
-    item_2,
-    item_3,
-    item_4,
-    item_5,
-    item_6,
-    item_7,
-    item_8,
-  ].filter((item) => typeof item !== "undefined");
-
-  count = arrayArround.reduce((acc, el, i) => {
-    acc += Boolean(el.isBomb) ? 1 : 0;
-    return acc;
-  }, 0);
-  arr[x][y].content = count;
-}
-
-export const getFinallyDomField = (array) => {
-  const arr = array.forEach((item) => {
-    item.forEach((itemInside) => {
-      const itemDom = document.createElement("div");
-      itemDom.innerHTML = itemInside.content;
-      field.append(itemDom);
-    });
-  });
-
-  return arr;
-};
-
-let matrixField = getMatrixField();
-// getFinallyDomField(matrixField);
- */
-
-// ----
 class matrixField extends App {
   constructor() {
     super();
@@ -123,11 +22,11 @@ class matrixField extends App {
       let current = arr[y][x];
 
       if (!current.isBomb) {
-        arr[y][x].content = "b";
+        arr[y][x].content = '💣';
         arr[y][x].isBomb = true;
         bombsCount--;
       } else {
-        arr[y][x].content = "";
+        arr[y][x].content = '';
       }
     }
   }
@@ -154,7 +53,7 @@ class matrixField extends App {
       item_6,
       item_7,
       item_8,
-    ].filter((item) => typeof item !== "undefined");
+    ].filter((item) => typeof item !== 'undefined');
 
     count = arrayArround.reduce((acc, el, i) => {
       acc += Boolean(el.isBomb) ? 1 : 0;
@@ -172,7 +71,7 @@ class matrixField extends App {
       let curr = [];
       for (let j = 0; j < width; j++) {
         curr[j] = new itemField(false, { i, j });
-        curr[j].content = "";
+        // curr[j].element.classList.add('field-item');
       }
       this.array.push(curr);
     }
@@ -181,7 +80,7 @@ class matrixField extends App {
 
     this.array.forEach((el, x) => {
       el.forEach((el, y) => {
-        if (el.content !== "b") {
+        if (el.content !== '💣') {
           this.getStateOfArroundItems(this.array, { x, y });
         }
       });
@@ -192,13 +91,25 @@ class matrixField extends App {
     this.getMatrixField(10, 10, 10);
     this.array.forEach((el) => {
       el.forEach((elTwo) => {
-        const item = document.createElement("div");
-        item.classList.add("field-item");
-        item.innerHTML = elTwo.content;
+        const item = document.createElement('div');
+        item.classList.add('field-item');
+        item.dataset.content = elTwo.content;
+        item.dataset.isBomb = elTwo.isBomb;
+        item.dataset.place = elTwo.place;
+        // item.innerHTML = item.dataset.content;
         this.field.append(item);
+        item.addEventListener('click', () => {
+          console.log(item.palce);
+
+          window.addEventListener('click', () => {
+            item.innerHTML = item.dataset.content;
+          });
+        });
       });
     });
   }
+
+  getContent() {}
 }
 
 const finallyArray = new matrixField();
--- minesweeper/js/fieldItem.js
+++ minesweeper/js/fieldItem.js
@@ -3,14 +3,25 @@ export class itemField {
     this.isBomb = isBomb;
     this.place = place;
     this.content = isBomb !== true ? '' : 'b';
+    this.element = '';
+
+    // window.addEventListener('click', () => {
+    //   this.element.innerHTML = this.content;
+    // });
   }
 
-  setBoxContent(value) {
-    this.content = value;
+  setBoxContent(value) {}
+
+  moveToBattlefield() {
+    const item = document.createElement('div');
+    item.classList.add('field-item');
+    this.field.append(item);
+    item.addEventListener('click', () => {
+      console.log(item.palce);
+    });
+    this.element = item;
+    item.addEventListener('click', () => {
+      item.innerHTML = item.dataset.content;
+    });
   }
 }
-
-// export function getItem(isBomb, place) {
-//   const newItem = new itemField(isBomb, place);
-//   return newItem;
-// }
